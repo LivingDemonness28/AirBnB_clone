3194fe0fdb8b531ffcc7b7601ce18373f86eb890
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-12-11 17:26:52 +0200

    0x00/models: base_models.py (exe) file updated nd

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,18 @@ from datetime import datetime
 class BaseModel:
     """Represents a Base model."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a BaseModel instance with uuid and timestamps."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == 'created_at' or k == 'updated_at':
+                    setattr(self, k, datetime.strptime(v, "%Y-%m-%dT%H:%S.%f"))
+                elif k != "__class__":
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return a string representation of the BaseModel instance.
