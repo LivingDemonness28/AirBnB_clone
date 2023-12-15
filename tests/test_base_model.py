28f8cf7ebf82797a495b8e85c9b79dbe7a8c6aeb
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-12-15 03:40:19 +0200

    0x00/tests: test_base_model.py file updated 2nd

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
+"""Defines unnitest for base_models.py
+
+Unittest classes:
+    BaseModel_instantiation
+    BaseModel_save
+    BaseModel_to_dict
+"""
+
+
+import unittest
+from datetime import datetime, timedelta
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+class BaseModel_save(unittest.TestCase):
+    """Unittests for save() method in BaseModel class"""
+
+    def test_save(self):
+        my_mod = BaseModel()
+        init_time = datetime.now()
+        sim_time_passed = timedelta(seconds=1)
+        nt = init_time + sim_time_passed
+        my_mod.created_at = init_time
+        my_mod.save()
