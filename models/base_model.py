2427b41e4aaa183b3c0965825388d3790a479a83
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-12-15 04:59:42 +0200

    0x00/models: base_models.py file updated 5th

--- models/base_model.py
+++ models/base_model.py
@@ -9,9 +9,16 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialize a BaseModel instance with uuid and timestamps."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == 'created_at' or k == 'updated_at':
+                    setattr(self, k, datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return a string representation of the BaseModel instance.
@@ -20,14 +27,14 @@ class BaseModel:
             str: String representation of the BaseModel instance.
         """
         class_name = self.__class__.__name__
-        return f"[{class_name}] ({self.id}) {self.__dict__}"
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def to_dict(self):
         """Returns a dict representation of the BaseModel instance."""
         res_dict = self.__dict__.copy()
         res_dict['__class__'] = self.__class__.__name__
-        res_dict['created_at'] = self.created_at.isoformat()
-        res_dict['updated_at'] = self.updated_at.isoformat()
+        res_dict['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        res_dict['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         return res_dict
 
     def save(self):
